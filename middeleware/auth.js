d4ab636f66b41bc7d99141b80bddc2dc4d5439f6
NizarNaser <nizarnaser17@gmail.com>
2025-04-16 19:25:09 +0200

    commeit12

--- middeleware/auth.js
+++ middeleware/auth.js
@@ -1,21 +1,22 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
+const authMiddeleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-const authMiddeleware =async(req,res,next)=>{
-    const {token} = req.headers;
-    if(!token){
-        return res.json({success:false,message:"Not Authorized Login Again"})
-    }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ success: false, message: "Not Authorized, Login Again" });
+  }
 
-    try {
-        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
-        req.body.userId = token_decode.id;
-        next();
-    } catch (error) {
-        console.log(error)
-        res.json({success:false,message:"Error"})
-    }
+  const token = authHeader.split(" ")[1]; // نفصل الكلمة Bearer عن التوكن الحقيقي
 
-}
+  try {
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log("JWT Error:", error.message);
+    res.status(403).json({ success: false, message: "Invalid or Expired Token" });
+  }
+};
 
 export default authMiddeleware;
