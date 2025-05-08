02857f425c23b571a22ad94107386083646ff1dc
NizarNaser <nizarnaser17@gmail.com>
2025-05-08 07:48:45 +0200

    first commit35

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -68,7 +68,6 @@ const listFood = async (req, res) => {
         res.json({ success: true, data: foods, cache: false });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ success: false, message: "Error fetching foods" });
     }
 };
 
