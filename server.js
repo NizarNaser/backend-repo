94057145c5c361480b8929c8d13aac6d9c0b8e66
NizarNaser <nizarnaser17@gmail.com>
2025-05-08 07:55:43 +0200

    first commit36

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const port =process.env.PORT
 app.use(express.json())
 // ✅ السماح فقط لموقع GitHub Pages
 const allowedOrigins = [
+  "http://localhost:5173",
   "https://dubai-restaurant-karaoke-bar.vercel.app",
   "https://nizarnaser.github.io"
 ];
